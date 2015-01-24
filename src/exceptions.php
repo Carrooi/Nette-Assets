2ba34049ce3922c1a70383c1660d8950d2fba844
David Kudera <sakren@gmail.com>
2015-01-24 15:09:28 +0100

    remove some unused exception classes

--- src/exceptions.php
+++ src/exceptions.php
@@ -4,20 +4,10 @@ namespace Carrooi\Assets;
 
 class RuntimeException extends \RuntimeException {}
 
-class LogicException extends \LogicException {}
-
-class IOException extends RuntimeException {}
-
-class PathNotFoundException extends IOException {}
-
-class DirectoryNotFoundException extends PathNotFoundException {}
-
-class InvalidStateException extends RuntimeException {}
-
 class InvalidArgumentException extends \InvalidArgumentException {}
-
-class NotImplementedException extends LogicException {}
 
 class AssetsNamespaceNotExists extends RuntimeException {}
 
 class AssetsResourceNotExists extends RuntimeException {}
+
+class InvalidStateException extends RuntimeException {}
